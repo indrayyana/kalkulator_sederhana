81972146a76f36b406bc64bdb55a347a472027a4
Indra Yana <gdindra13@gmail.com>
2022-12-23 16:27:41 +0800

    update 2.0

--- script.js
+++ script.js
@@ -13,9 +13,6 @@ function kalkulator(bil_1, bil_2, operator) {
     case "/":
       hasil = Number(bil_1) / Number(bil_2);
       break;
-    case "**":
-      hasil = Number(bil_1) ** Number(bil_2);
-      break;
   }
 
   document.getElementById("hasil").innerHTML = hasil;
